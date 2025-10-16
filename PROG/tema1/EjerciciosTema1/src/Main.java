aa7dd45b886fcb56e4b69c6b434e1bf4f9f422fd
jessicaCH10 <jessica.1010ch@gmail.com>
2025-10-16 22:05:24 +0200

    update

--- PROG/tema1/EjerciciosTema1/src/Main.java
+++ PROG/tema1/EjerciciosTema1/src/Main.java
@@ -113,12 +113,12 @@ public class Main {
       int multiplicacion = numero1 *numero2;
       int division = (numero1/numero2);
 
-      System.out.println("Número 1 " + numero1);
-      System.out.println("Número 2 " + numero2);
-      System.out.println("Suma " + suma);
-      System.out.println("Resta " + resta);
-      System.out.println("Multiplicación " + multiplicacion);
-      System.out.println("División " + division);
+      System.out.println("Número 1 = " + numero1);
+      System.out.println("Número 2 =" + numero2);
+      System.out.println("Suma =" + suma);
+      System.out.println("Resta =" + resta);
+      System.out.println("Multiplicación =" + multiplicacion);
+      System.out.println("División =" + division);
   }
 
 
