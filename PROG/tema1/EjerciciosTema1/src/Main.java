db1968946e9f7c46a343a22678593d6ba93202e3
jessicaCH10 <jessica.1010ch@gmail.com>
2025-10-16 22:01:14 +0200

    update

---
+++ PROG/tema1/EjerciciosTema1/src/Main.java
@@ -0,0 +1,148 @@
+public class Main {
+
+
+
+
+
+
+
+  public static void  main (String[] args) {
+
+/*ejercicio 1
+
+      int edad1 = 25;
+      String nombre = "Ana";
+      String ciudad ="Madrid";
+
+      System.out.println(edad1);
+      System.out.println(nombre);
+      System.out.println(ciudad);*/
+
+
+
+/*ejercicio 2
+    int puntuacion= 0;
+    int primeraModificacion= puntuacion + 5;
+    int segundaModificacion= puntuacion + 10;
+      int puntuacionFinal= puntuacion + 15;
+    System.out.println(puntuacion);
+
+      System.out.println("la primera modificación es "+primeraModificacion);
+      System.out.println("la segunda modificación es "+segundaModificacion);
+      System.out.println("la puntuación final es "+puntuacionFinal);*/
+
+/*ejercicio 3
+
+      String nombre = "Carlos";
+      int edad3 =30;
+      boolean estudiante = true;
+      double altura =1.75;
+      char inicial = 'C';
+      System.out.println("Nombre "+nombre +"- Tipo: String");
+      System.out.println("Edad "+edad3 +"- Tipo: int");
+      System.out.println("¿Es estudiante? "+estudiante +"- Tipo: boolean");
+      System.out.println("Altura "+altura +"- Tipo: double");
+      System.out.println("Inicial "+inicial+"- Tipo: char");*/
+
+/*EJERCICIO 4
+      String titulo = "Don Quijote de la Mancha";
+      String autor= "Miguel de Cervantes";
+      int año =1605;
+      int paginas =863;
+      boolean disponible = true;
+       System.out.println("Titulo " +titulo);
+      System.out.println("Autor "+autor);
+      System.out.println("¿Esta disponible en la biblioteca? "+disponible);
+      System.out.println("Páginas "+paginas);
+      System.out.println("Año"+año);*/
+
+
+      //ejercicio5//
+      /*final String NUMERO_PI = "3.141509";
+      final String NOMBRE_APP = "MIAPP";
+      final String VERSION = "1.0.0";
+      String usuario = "Laura";
+      usuario ="Miguel";
+      String nivel = "1";
+      nivel ="2";
+      String puntuacion= "0";
+      puntuacion ="150";
+      System.out.println("Aplicación " +NOMBRE_APP);
+      System.out.println("Versión "+VERSION);
+      System.out.println("Valor de pi "+NUMERO_PI);
+      System.out.println("Usuario actual "+usuario);
+      System.out.println("Nivel " +nivel);
+      System.out.println("Puntuación "+puntuacion);
+      System.out.println("Usuario actualizado "+usuario);
+      System.out.println("Nivel actualizado " +nivel);
+      System.out.println("Puntuación actualizada "+puntuacion);*/
+      //otra opcion
+   /*   final String NUMERO_PI = "3.141509";
+      final String NOMBRE_APP = "MIAPP";
+      final String VERSION = "1.0.0";
+
+      String usuario = "Laura";
+      String nivel = "1";
+      String puntuacion = "0";
+
+      // Mostrar valores iniciales
+      System.out.println("Aplicación: " + NOMBRE_APP);
+      System.out.println("Versión: " + VERSION);
+      System.out.println("Valor de pi: " + NUMERO_PI);
+      System.out.println("Usuario actual: " + usuario);
+      System.out.println("Nivel: " + nivel);
+      System.out.println("Puntuación: " + puntuacion);
+
+      // Actualizar valores
+      usuario = "Miguel";
+      nivel = "2";
+      puntuacion = "150";
+
+      // Mostrar valores actualizados
+      System.out.println("Usuario actualizado: " + usuario);
+      System.out.println("Nivel actualizado: " + nivel);
+      System.out.println("Puntuación actualizada: " + puntuacion);*/
+
+
+//Ejercicio 6
+
+      int numero1 = 10;
+      int numero2 = 5;
+      int suma = numero1 +numero2;
+      int resta = numero1 -numero2;
+      int multiplicacion = numero1 *numero2;
+      int division = (numero1/numero2);
+
+      System.out.println("Número 1 " + numero1);
+      System.out.println("Número 2 " + numero2);
+      System.out.println("Suma " + suma);
+      System.out.println("Resta " + resta);
+      System.out.println("Multiplicación " + multiplicacion);
+      System.out.println("División " + division);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
