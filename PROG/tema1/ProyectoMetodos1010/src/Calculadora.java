c7f84914c4aaca46c7ae4695af8673be749faa34
jessicaCH10 <jessica.1010ch@gmail.com>
2025-10-22 12:34:52 +0200

    update

---
+++ PROG/tema1/ProyectoMetodos1010/src/Calculadora.java
@@ -0,0 +1,24 @@
+public class Calculadora {
+
+
+    public static void sumar(int op1, int op2) {
+        int resultado = op1 + op2;
+        System.out.println("El resultado de la suma es " + resultado);
+
+    }
+
+    public static void restar (int op1, int op2) {
+        int resultado = op2-op1;
+        System.out.println("El resultado de la suma es " + resultado);
+
+    }
+
+
+    public static void multiplicar (int op1, int op2) {
+        int resultado = op2*op1;
+        System.out.println("El resultado de la suma es " + resultado);
+
+    }
+
+
+}
---
+++ PROG/tema1/ProyectoMetodos1010/src/Entrada.java
@@ -0,0 +1,33 @@
+public class Entrada {
+
+
+
+    public static void main (String[] args){
+
+
+        System.out.println("Proyecto Métodos");
+        System.out.println("vamos a realizar llamadas a Métodos");
+
+        int numero;
+        Calculadora operacionesMatematicas = new Calculadora();
+
+      Calculadora.sumar(2,4);
+      Calculadora.restar(5,5);
+      Calculadora.multiplicar(2,2);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
