c3fc7e420b944299ef8214c1282341ab261658c1
jessicaCH10 <jessica.1010ch@gmail.com>
2025-10-24 12:26:13 +0200

    update

---
+++ PROG/tema2/EjercTema2/src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando programa de operadores");
+        Operadores operacionesVariable = new Operadores();// operadores nombre de la clase
+        operacionesVariable.operadoresAritmeticos();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ PROG/tema2/EjercTema2/src/Operadores.java
@@ -0,0 +1,20 @@
+public class Operadores {
+
+    public void operadoresAritmeticos() {
+
+        // unarios
+        int operandoUno = 5;
+        // incremento -> para poder hacer un contador
+        operandoUno++; // le suma uno al valor que ya tenia
+        operandoUno++;
+        operandoUno++;
+
+        System.out.println("El valor del operando uno despues de los incrementos es " + operandoUno);
+
+
+    }
+
+
+
+
+}
